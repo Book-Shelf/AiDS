3d2e788a17bc6b779588c127abe75df7a04954fe
Damian <arturadka@gmail.com>
2021-04-11 18:06:03 +0200

    Update graph.cpp
    Temprorary tester file

--- Lab4/graph.cpp
+++ Lab4/graph.cpp
@@ -4,5 +4,15 @@ int main() {
 
     Graph graph;
     graph.addEdge(1, 2);
+    graph.addEdge(1, 3);
+    graph.addEdge(3, 2);
+    graph.addEdge(2, 1);
+    graph.addEdge(2, 1);
+    graph.addEdge(2, 3);
+    graph.print();
+
+    graph.removeEdge(2, 1);
+    graph.removeEdge(2, 1);
+    graph.removeEdge(1, 2);
     graph.print();
 }
