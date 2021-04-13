f1a593226f1d15956554432a3ddb1815394ab9f3
Damian <arturadka@gmail.com>
2021-04-13 19:52:31 +0200

    Added DFS method

--- Lab4/graph.hpp
+++ Lab4/graph.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <list>
 #include <queue>
+#include <stack>
 
 int max(int a, int b) { 
 
@@ -16,30 +17,50 @@ struct Node {
     Node* next;
     int weight;
     int dist;                   // distance to the root. Only positive numbers when root is set.
+    int colour;                 // 0 white, 1 gray, 2 black
+    int gTime;                  // visited time
+    int bTime;                  // time when all neighbours were processed
+    int parent;
 
     Node()
         : key(1)
         , next(nullptr) 
         , weight(1) 
-        , dist(-1) {}
+        , dist(-1) 
+        , colour(0) 
+        , gTime(0)
+        , bTime(0)
+        , parent(-1) {}
 
     Node(int key)
         : key(key)
         , next(nullptr) 
         , weight(1) 
-        , dist(-1) {}
+        , dist(-1) 
+        , colour(0)
+        , gTime(0)
+        , bTime(0)
+        , parent(-1) {}
 
     Node(int key, Node* next) 
         : key(key)
         , next(next) 
         , weight(1) 
-        , dist(-1) {}
+        , dist(-1) 
+        , colour(0)
+        , gTime(0)
+        , bTime(0)
+        , parent(-1) {}
     
     Node(int key, int weight) 
         : key(key)
         , next(nullptr)
         , weight(weight)
-        , dist(-1) {}
+        , dist(-1) 
+        , colour(0)
+        , gTime(0)
+        , bTime(0)
+        , parent(-1) {}
         
 };
 
@@ -51,6 +72,11 @@ class Graph  {
     int root;                           // when transformed into a tree using BFS
 
     void setDistance(int vertex, int distance);
+    void setColour(int vertex, int colour);
+    void setBTime(int vertex, int time);
+    void setGTime(int vertex, int time);
+    int DFS_Visit(Graph& graph, int vertex, int time, Graph& tree);
+    void setParent(int vertex, int parent);
     
     public:
 
@@ -69,6 +95,7 @@ class Graph  {
     static Graph BFS(Graph& graph, int start);    
     std::list<int> get_Path(int end);           // path from the root to the end vertex
     int get_hops(int end);                // distance between root and end vertex
+    static Graph DFS(Graph& graph);
 };
 
 Graph::Graph()
@@ -334,6 +361,81 @@ std::list<int> Graph::get_Path(int end) {
 int Graph::get_hops(int end) {
 
     return end <= numOfVertices ? verticies[end - 1].dist : -1;
-} 
+}
+
+
+void Graph::setColour(int vertex, int colour) {
+
+    verticies[vertex - 1].colour = colour;
+}
+
+
+void Graph::setBTime(int vertex, int time) {
+
+    verticies[vertex - 1].bTime = time;
+}
+
+
+void Graph::setGTime(int vertex, int time) {
+
+    verticies[vertex - 1].gTime = time;
+}
+
+
+void Graph::setParent(int vertex, int parent) {
+
+    verticies[vertex - 1].parent = parent;
+}
+
+
+int Graph::DFS_Visit(Graph& graph, int vertex, int time, Graph& tree) {
+
+    time += 1;
+    graph.setColour(vertex, 1);
+    graph.setGTime(vertex, time);
+    Node* temp = graph.getVertex(vertex - 1).next;
+
+    while (temp != nullptr) {
+
+        if (graph.getVertex(temp->key - 1).colour == 0) {
+
+            tree.addEdge(vertex, temp->key);
+            graph.setParent(temp->key, vertex);
+            time = DFS_Visit(graph, temp->key, time, tree);
+        }
+
+        temp = temp->next;
+    }
+
+    graph.setColour(vertex, 2);
+    time += 1;
+    graph.setBTime(vertex, time);
+
+    return time;
+}
+
+
+Graph Graph::DFS(Graph& graph) {
+
+    for (int i = 1; i <= graph.size(); ++i) {
+        graph.setColour(i, 0);
+    }
+
+    Graph tree(graph.size());
+    int time = 0;
+
+    for (int i = 1; i <= graph.size(); ++i) {
+
+        if (graph.getVertex(i).colour == 0) {
+
+            time = graph.DFS_Visit(graph, i, time, tree);
+        }
+    }
+
+    std::cout << time << '\n';
+
+    return tree;
+
+}
 
 #endif
