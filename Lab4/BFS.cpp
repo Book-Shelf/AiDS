de3f991798ae02a2a2d8fda2a07252188548fc9d
Damian <arturadka@gmail.com>
2021-04-13 13:58:34 +0200

    Added BFS file
    File that contains converter from graph to dot extension

---
+++ Lab4/BFS.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include "graph.hpp"
+
+// Function to convert graph into dot format
+// directed := 1(directed)/ 2(undirected)
+void toDot(Graph &graph, bool directed) {
+
+    std::ofstream image("graph.dot");
+
+    std::string graphType = directed ? "digraph" : "graph";
+    std::string graphConnection = directed ? "->" : "--";
+
+    image << graphType << "{\n";
+    for (int i = 1; i <= graph.size(); ++i) {
+        for (int j = 1; j <= graph.size(); ++j) {
+            
+            if (graph.isEdge(i, j)) {
+                image << "\t" << i << " " << graphConnection << " " << j << ";\n";
+            }
+
+        }
+    }
+    image << "}";
+
+    image.close();
+}
+
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+
+        std::cerr << "Type path to file";
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    std::ifstream data(filename);
+    std::string VEW[3]; // start Vertes, end Vertex, weigth
+    Graph graph;
+
+    while(std::getline(data, VEW[0], ',')) {
+        
+        std::getline(data, VEW[1], ',');
+        std::getline(data, VEW[2]);
+        graph.addEdge(stoi(VEW[0]), stoi(VEW[1]));
+    }
+
+    data.close();
+    toDot(graph, 1);
+}
