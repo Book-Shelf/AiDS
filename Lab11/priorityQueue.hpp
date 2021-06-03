b81e6c05b21b20faac1ed9007f99a6f38393c2d1
Damian <arturadka@gmail.com>
2021-06-03 21:22:26 +0200

    added pq file

---
+++ Lab11/priorityQueue.hpp
@@ -0,0 +1,65 @@
+#ifndef PRIORITYQUEUE_HXX
+#define PRIORITYQUEUE_HXX
+#include "heap.hpp"
+
+template<class T>
+class PQueue {
+
+    typedef std::pair<T, int> element;
+    
+    private:
+        Heap<T> heap;
+    
+    public:
+
+    PQueue() : heap(*new Heap<T>) {}
+
+    // Constructor: build heap based on list of elements
+    PQueue(std::vector<element> elements) {
+
+        heap.build(elements);
+    }
+
+    PQueue(int size) : heap(size) {}
+
+    void InsertElement(element e) {
+        heap.push(e);
+    }
+
+    int getMinPriority(){
+        return heap.rootElement().second;
+    }
+
+    element Pop() {
+        return heap.pop();
+    }
+
+    void IncreasePriority(element e, int priority) {
+
+        heap.checkAndIncrease(e, priority);
+    }
+
+    // prints priorities from the lowest priority to the top
+    void printHeap() {
+
+        heap.printHeap();
+    }
+
+    int size() {
+
+        return heap.size();
+    }
+
+    void check() {
+
+        std::cout << heap.check() << "\n";
+    }
+
+    bool empty() {
+
+        return heap.empty();
+    }
+};
+
+
+#endif
