12746bcb3f0806176241123fae7055d7e9a98752
Damian <arturadka@gmail.com>
2021-06-04 14:31:17 +0200

    Changed dane to entrydata

--- Lab12/Ex1/przemytnicy.cpp
+++ Lab12/Ex1/przemytnicy.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[]) {
 
     if (argc != 2) {
-        std::cerr << "[dane.csv]\n";
+        std::cerr << "[entryData.csv]\n";
         return 1;
     }
 
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]) {
 
     std::string filename = argv[1];
     std::ifstream data(filename);
-    std::string dane;
+    std::string entryData;
     static int rowNumber = 0;
     static int numberOfMetals = -100000;
     static int* metalsPrices;
@@ -32,8 +32,8 @@ int main(int argc, char* argv[]) {
 
             case METAL_TYPE:
 
-                std::getline(data, dane, '\n');
-                numberOfMetals = std::stoi(dane);
+                std::getline(data, entryData, '\n');
+                numberOfMetals = std::stoi(entryData);
                 std::cout << "NumberOfMetals: " << numberOfMetals << "\nTheir prices: {";
                 metalsPrices = new int[numberOfMetals];
 
@@ -42,28 +42,28 @@ int main(int argc, char* argv[]) {
             
             case METAL_PRICE:
 
-                std::getline(data, dane, '\n');
-                metalsPrices[rowNumber - 2] = std::stoi(dane);
+                std::getline(data, entryData, '\n');
+                metalsPrices[rowNumber - 2] = std::stoi(entryData);
                 std::cout << metalsPrices[rowNumber - 2] << ", ";
                 if (rowNumber == (numberOfMetals + 1)) entryType = KNOWN_PROCESSES;
                 break;
 
             case KNOWN_PROCESSES:
 
-                std::getline(data, dane, '\n');
-                numberOfKnownProcesses = std::stoi(dane);
+                std::getline(data, entryData, '\n');
+                numberOfKnownProcesses = std::stoi(entryData);
                 std::cout << "\b\b}\nKnown number of transmutations: " << numberOfKnownProcesses << "\n";
                 entryType = TRANSMUTATION_PRICES;
                 break;
 
             case TRANSMUTATION_PRICES:
 
-                std::getline(data, dane, ' ');
-                fromMetal = std::stoi(dane);
-                std::getline(data, dane, ' ');
-                toMetal = std::stoi(dane);
-                std::getline(data, dane, '\n');
-                price = std::stoi(dane);
+                std::getline(data, entryData, ' ');
+                fromMetal = std::stoi(entryData);
+                std::getline(data, entryData, ' ');
+                toMetal = std::stoi(entryData);
+                std::getline(data, entryData, '\n');
+                price = std::stoi(entryData);
                 std::cout << "Transmutation from metal: (" << fromMetal << ") To metal: (" << toMetal << ") Costs: " << price << "\n";
                 break;
 
