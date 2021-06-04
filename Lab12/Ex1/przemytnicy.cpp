b0db08bf91bf102d3358ddd6e666d8460204d1f3
Damian <arturadka@gmail.com>
2021-06-04 16:01:31 +0200

    Added graph implementation with dijkstra algorithm

--- Lab12/Ex1/przemytnicy.cpp
+++ Lab12/Ex1/przemytnicy.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include "graph.hpp"
 
-void readFromFile(std::string filename) {
+Graph readFromFile(std::string filename, int& numberOfMetals, int* metalsPrices, int& numberOfKnownProcesses, int& fromMetal, int& toMetal, int& price) {
 
     enum Entry {METAL_TYPE = 0, METAL_PRICE, KNOWN_PROCESSES, TRANSMUTATION_PRICES} entryType; 
 
     entryType = METAL_TYPE;
     std::ifstream data(filename);
     std::string entryData;
-    static int rowNumber = 0;
-    static int numberOfMetals = -100000;
-    static int* metalsPrices;
-    static int numberOfKnownProcesses = -100000;
-    static int fromMetal = 0, toMetal = 0, price = 0;
+    Graph transmutationGraph(true);
+
+    int rowNumber = 0;
 
 
     while(1) {
@@ -58,12 +57,14 @@ void readFromFile(std::string filename) {
                 std::getline(data, entryData, '\n');
                 price = std::stoi(entryData);
                 std::cout << "Transmutation from metal: (" << fromMetal << ") To metal: (" << toMetal << ") Costs: " << price << "\n";
+                transmutationGraph.addEdge(fromMetal, toMetal, price);
                 break;
-
         }
     }
 
     data.close();
+
+    return transmutationGraph;
 }
 
 int main(int argc, char* argv[]) {
@@ -74,7 +75,11 @@ int main(int argc, char* argv[]) {
     }
 
     std::string filename = argv[1];
+    int numberOfMetals = -100000;
+    int* metalsPrices = new int[0];
+    int numberOfKnownProcesses = -100000;
+    int fromMetal = 0, toMetal = 0, price = 0;
+    Graph transmutationGraph = readFromFile(filename, numberOfMetals, metalsPrices, numberOfKnownProcesses, fromMetal, toMetal, price);
 
-    readFromFile(filename);
-   
+    transmutationGraph.print();
 }
